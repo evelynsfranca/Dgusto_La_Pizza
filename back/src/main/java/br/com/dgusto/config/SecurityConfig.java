94373926b25647c6bf35c9a54df990c931bf83f1
Evelyn França <evelynsfrancaa@gmail.com>
2021-09-16 22:18:10 -0300

    small fixes

--- back/src/main/java/br/com/dgusto/config/SecurityConfig.java
+++ back/src/main/java/br/com/dgusto/config/SecurityConfig.java
@@ -29,8 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests()
-                .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
-            .antMatchers(HttpMethod.POST, "/api/signup").permitAll()
+            .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
             .antMatchers("/api/admin/**").hasRole("ADMIN")
             .antMatchers("/api/user/**").hasRole("USER")
             .antMatchers("/api/client/**").hasRole("CLIENT")
