06232aab24e40e4ee22ff51c72472642748e9579
Felipe Luis <felipeluis.6@gmail.com>
2021-09-28 15:21:09 -0300

    feat: add delete btn for product list

--- front/pages/admin/products/list.js
+++ front/pages/admin/products/list.js
@@ -2,55 +2,85 @@ import { useRouter } from 'next/dist/client/router';
 import Head from 'next/head';
 import { useEffect, useState } from "react";
 import NumberFormat from 'react-number-format';
+import SweetAlert from 'react-bootstrap-sweetalert';
 import useSWR from 'swr';
 import LayoutAdmin from '../../../components/layout/admin';
 import { API_URL } from '../../../utils/constants';
 
-function ApiProductsList({ token }) {
-
-  if (!token) return <></>
-
-  const router = useRouter();
-
-  const fetcher = (url, token) => fetch(url, { headers: { "Authorization": token } })
-    .then(res => res.json())
-    .catch(e => console.warn(e))
-
-  const { data, error } = useSWR([`${API_URL}/admin/products`, token], fetcher)
-
-  if (error) return <tr rowSpan="4"><td>failed to load</td></tr>
-  if (!data) return <tr rowSpan="4"><td>loading...</td></tr>
-
-  return <>
-    {data.content.map((product, index) => (
-      <tr key={index}>
-        <td>
-          {product.name}
-        </td>
-        <td>
-          <NumberFormat value={product.unitValue} displayType={'text'} thousandSeparator={true} prefix={'R$'} />
-        </td>
-        <td>
-          {product.stockQuantity}
-        </td>
-        <td>
-          <button type="button" className="button-secondary" onClick={() => router.push(`/admin/products/detail/${product.id}`)} title="Visualizar">
-            Ver
-          </button>
-          {' '}
-          <button type="button" className="button-tertiary" onClick={() => router.push(`/admin/products/update/${product.id}`)} title="Editar">
-            Editar
-          </button>
-        </td>
-      </tr>
-    ))}
-  </>
-}
-
-
 export default function ProductList() {
   const router = useRouter();
   const [token, setToken] = useState('');
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [productToDeleteModal, setProductToDeleteModal] = useState(false);
+
+  function openModalDelete(productId) {
+    setProductToDeleteModal(productId)
+    setShowDeleteModal(true)
+  }
+
+  async function confirmDelete() {
+    const res = await fetch(`${API_URL}/admin/products/${productToDeleteModal}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": token
+      }
+    })
+      .then(res => res)
+      .catch(e => console.warn(e));
+
+    const response = await res;
+
+    if (!!response) {
+      setShowDeleteModal(false)
+      location.reload();
+    }
+  }
+
+  function ApiProductsList({ token }) {
+
+    if (!token) return <></>
+
+    const router = useRouter();
+
+    const fetcher = (url, token) => fetch(url, { headers: { "Authorization": token } })
+      .then(res => res.json())
+      .catch(e => console.warn(e))
+
+    const { data, error } = useSWR([`${API_URL}/admin/products`, token], fetcher)
+
+    if (error) return <tr rowSpan="4"><td>failed to load</td></tr>
+    if (!data) return <tr rowSpan="4"><td>loading...</td></tr>
+
+    return <>
+      {data.content.map((product, index) => (
+        <tr key={index}>
+          <td>
+            {product.name}
+          </td>
+          <td>
+            <NumberFormat value={product.unitValue} displayType={'text'} thousandSeparator={true} prefix={'R$'} />
+          </td>
+          <td>
+            {product.stockQuantity}
+          </td>
+          <td>
+            <button type="button" className="button-secondary" onClick={() => router.push(`/admin/products/detail/${product.id}`)} title="Visualizar">
+              Ver
+            </button>
+            {' '}
+            <button type="button" className="button-tertiary" onClick={() => router.push(`/admin/products/update/${product.id}`)} title="Editar">
+              Editar
+            </button>
+            {' '}
+            <button type="button" className="button-danger" onClick={() => openModalDelete(product.id)} title="Deletar">
+              Deletar
+            </button>
+          </td>
+        </tr>
+      ))}
+    </>
+  }
 
   useEffect(() => {
     if (typeof window !== undefined && localStorage.getItem('token')) {
@@ -84,6 +114,21 @@ export default function ProductList() {
         </thead>
         <tbody>
           <ApiProductsList token={token} />
+
+          <SweetAlert
+            show={showDeleteModal}
+            warning
+            showCancel
+            cancelBtnText="Cancelar"
+            confirmBtnText="Deletar"
+            confirmBtnBsStyle="danger"
+            title="Deseja mesmo deletar?"
+            onConfirm={() => confirmDelete()}
+            onCancel={() => setShowDeleteModal(false)}
+            focusCancelBtn
+          >
+            Ao confirmar, você irá deletar o produto, esta ação não poderá ser revertida.
+          </SweetAlert>
         </tbody>
       </table>
 
