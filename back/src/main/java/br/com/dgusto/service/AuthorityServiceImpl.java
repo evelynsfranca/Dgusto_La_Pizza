941db4edf87e0595007850863f09b8b8c0703883
Evelyn França <evelynsfrancaa@gmail.com>
2021-10-30 15:57:19 -0300

    small refactor

--- back/src/main/java/br/com/dgusto/service/AuthorityServiceImpl.java
+++ back/src/main/java/br/com/dgusto/service/AuthorityServiceImpl.java
@@ -2,7 +2,9 @@ package br.com.dgusto.service;
 
 import br.com.dgusto.domain.Authority;
 import br.com.dgusto.repository.AuthorityRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class AuthorityServiceImpl implements AuthorityService {
@@ -15,6 +17,7 @@ public class AuthorityServiceImpl implements AuthorityService {
 
     @Override
     public Authority findById(String id) {
-        return authorityRepository.findById(id).orElseThrow();
+        return authorityRepository.findById(id)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "error.authority.notFound"));
     }
 }
