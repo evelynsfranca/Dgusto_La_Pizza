d3dc32bc925f7498cd303d8cfc13369a7d2b3d0a
Felipe Luis <felipeluis.6@gmail.com>
2021-11-12 22:21:56 -0300

    feat: myaccount my pushases page create account

--- front/src/components/MyAccountData/myAccountData.tsx
+++ front/src/components/MyAccountData/myAccountData.tsx
@@ -1,47 +1,107 @@
-import useSWR from 'swr';
-import { API_URL } from '../../utils/constants';
-import CurrencyFormat from 'react-currency-format';
-import Loading from '../Loading/loading';
-import { useState } from 'react';
+import useSWR from 'swr'
+import { API_URL } from '../../utils/constants'
+import CurrencyFormat from 'react-currency-format'
+import Loading from '../Loading/loading'
+import { ReactElement, useEffect, useState } from 'react'
+import Link from 'next/link'
+import _ from 'lodash'
 
 export interface IContentPromotions {
-  content: IPromotions[];
+  content: IPromotions[]
 }
 
 export interface IPromotions {
-  id: Number | any;
-  name: String;
-  description?: String;
-  unitValue: Number;
+  id: Number | any
+  name: String
+  description?: String
+  unitValue: Number
+}
+
+export const UserInfos = (data): any => {
+  return (
+    _.map(data, account => (
+      <div key={account.id}>
+        <h1>cpf: {account.cpf}</h1>
+        <h1>nome: {account.user.name}</h1>
+        <h1>email: {account.user.email}</h1>
+        <h1>telefones: {account.phones}</h1>
+        <h1>endereços: {account.addresses}</h1>
+      </div>
+    ))
+  )
 }
 
 export function MyAccountData() {
-  const fetcher = (url, token = localStorage.getItem('token')) => fetch(url, { headers: { "Authorization": token } })
-    .then(res => res.json())
-    .catch(e => console.warn(e))
+  const [userData, setUserData] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [isErrored, setIsErrored] = useState('')
 
-  const { data, error } = useSWR(`${API_URL}/client/me`, fetcher)
+  const getData = async () => {
+    setIsLoading(true)
 
-  if (error) return <>failed to load</>
-  if (!data) return <Loading />
+    const res = await fetch(`${API_URL}/client/me`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": localStorage.getItem('token')
+      }
+    })
+      .then(res => res.json())
+      .catch(e => console.warn(e))
 
-  if (data.status != 200) return <div className="alert alert-danger" role="alert">Erro ao tentar carregar seus dados, tente novamente mais tarde. </div>
+    const response = await res
+
+    if (response) {
+      setUserData(response)
+    } else {
+      setIsErrored('Erro ao tentar carregar seus dados, tente novamente mais tarde.');
+    }
+
+    setIsLoading(false)
+
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+
+  // const fetcher = (url, token = localStorage.getItem('token')) => fetch(url, { headers: { "Authorization": token } })
+  //   .then(res => res.json())
+  //   .catch(e => console.warn(e))
+
+  // const { data, error } = useSWR(`${API_URL}/client/me`, fetcher)
+
+  // if (error) return <>failed to load</>
+  // if (!data) return <Loading />
+
+  // if (!data?.cpf) {
+  //   let message: ReactElement
+  //   message = <div className="alert alert-danger" role="alert">Erro ao tentar carregar seus dados, tente novamente mais tarde.</div>
+  //   return message
+  // }
+
+  // setUserData(data)
 
   return (
     <>
       <div className="container">
         <div className="row my-5 py-5">
 
-          {data?.content?.map(request => (
-            <div key={request.id}>
-              <h1>{request.name}</h1>
-            </div>
-          ))}
+          {!isLoading && isErrored &&
+            <>
+              <div className="alert alert-danger" role="alert">
+                Erro ao tentar carregar seus dados, tente novamente mais tarde.
+              </div>
+            </>
+          }
+
+          {!isLoading && !!userData && <UserInfos data={userData} />}
 
         </div>
       </div>
     </>
-  );
+  )
 }
 
-export default MyAccountData;
+export default MyAccountData
--- front/src/pages/create-account.tsx
+++ front/src/pages/create-account.tsx
@@ -5,6 +5,7 @@ import LayoutGeneral from 'src/components/Layout/layoutGeneral';
 import { API_URL } from 'src/utils/constants';
 
 export interface IUser {
+  cpf: string;
   name: string;
   email: string;
   password: string;
@@ -17,6 +18,7 @@ function CreateAccountPage({ cartData }) {
   const [token, setToken] = useState('');
   const [sendingForm, setSendingForm] = useState<boolean>(false)
 
+  const [isCpfInvalid, setIsCpfInvalid] = useState<boolean>(false);
   const [isNameInvalid, setIsNameInvalid] = useState<boolean>(false);
   const [isEmailInvalid, setIsEmailInvalid] = useState<boolean>(false);
   const [isPasswordInvalid, setIsPasswordInvalid] = useState<boolean>(false);
@@ -24,6 +26,7 @@ function CreateAccountPage({ cartData }) {
   const [errorMessage, setErrorMessage] = useState<string>(undefined);
 
   const [user, setUser] = useState<IUser>({
+    cpf: '',
     name: '',
     email: '',
     password: '',
@@ -37,6 +40,7 @@ function CreateAccountPage({ cartData }) {
   }, []);
 
   function resetStatusForm() {
+    setIsCpfInvalid(false);
     setIsNameInvalid(false);
     setIsEmailInvalid(false);
     setIsPasswordInvalid(false);
@@ -49,6 +53,16 @@ function CreateAccountPage({ cartData }) {
 
   function validateForm(): boolean {
     let returnValue = false;
+
+    if (user.email.length === 0) {
+
+      setIsCpfInvalid(true);
+      returnValue = true;
+
+    } else {
+      resetStatusForm()
+      returnValue = false;
+    }
 
     if (user.email.length < 3) {
 
@@ -167,8 +181,8 @@ function CreateAccountPage({ cartData }) {
 
     } else {
 
-      const res = await fetch(`${API_URL}/create-account`, {
-        method: "PUT",
+      const res = await fetch(`${API_URL}/signup`, {
+        method: "POST",
         headers: {
           "Content-Type": "application/json",
           "Authorization": token
@@ -180,8 +194,12 @@ function CreateAccountPage({ cartData }) {
 
       const response = await res;
 
-      if (response) {
+      if (response.ok) {
         router.push('/login')
+      }
+
+      if (response.status != 200) {
+        setErrorMessage(response.error)
       }
 
       setSendingForm(false)
@@ -201,6 +219,19 @@ function CreateAccountPage({ cartData }) {
         }
 
         <form className="form">
+
+          <div className="form-floating mb-3">
+            <input
+              type="text"
+              className={[isCpfInvalid ? " is-invalid " : "", "form-control"].join(" ")}
+              value={user.cpf}
+              maxLength={11}
+              onChange={cpf => setUser({ ...user, cpf: cpf.target.value })}
+            />
+            <label>
+              CPF
+            </label>
+          </div>
 
           <div className="form-floating mb-3">
             <input
