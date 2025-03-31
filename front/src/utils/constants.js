4de88334972fe9c24f42f12bff7602d0c02d1d28
Evelyn França <evelynsfrancaa@gmail.com>
2025-03-31 10:15:26 -0300

    fix: front - env variables

--- front/src/utils/constants.js
+++ front/src/utils/constants.js
@@ -1,5 +1,5 @@
-export const API_URL = process.env.API_URL ?? "http://localhost:8080/api"
-export const API_LOGIN_URL = process.env.API_LOGIN_URL ?? "http://localhost:8080/api/auth/login"
+export const API_URL = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8080/api"
+export const API_LOGIN_URL = process.env.NEXT_PUBLIC_API_LOGIN_URL ?? "http://localhost:8080/api/auth/login"
 
 export const userAuthorities = [ 'ROLE_ADMIN', 'ROLE_USER',  'ROLE_EMPLOYEE',  'ROLE_CLIENT']
 
